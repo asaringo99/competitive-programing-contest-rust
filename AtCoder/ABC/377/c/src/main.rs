b49b72f76e426d937a59d8fc10e27110ce6d679f
asaringo <asaringo99@gmail.com>
2024-10-28 02:18:43 +0900

    test

---
+++ AtCoder/ABC/377/c/src/main.rs
@@ -0,0 +1,59 @@
+use text_io::read;
+
+macro_rules! scan {
+    ($($var:ident),+ $(,)?) => {
+        $(
+            $var = read!();
+        )+
+    };
+}
+
+macro_rules! scanv {
+    ($len:expr, $vec:ident, $ty:ty) => {
+        {
+            $vec = vec![];
+            for _ in 0..$len {
+                let value: $ty = read!();
+                $vec.push(value);
+            }
+        }
+    };
+}
+
+macro_rules! pt {
+    ($($arg:expr),*) => {
+        {
+            let mut iter = vec![$($arg.to_string()),*].into_iter();
+            if let Some(first) = iter.next() {
+                print!("{}", first);
+                for item in iter {
+                    print!(" {}", item);
+                }
+            }
+            println!();
+        }
+    };
+}
+
+macro_rules! ptv {
+    ($vec:expr) => {
+        {
+            let mut iter = $vec.iter();
+            if let Some(first) = iter.next() {
+                print!("{}", first);
+                for item in iter {
+                    print!(" {}", item);
+                }
+            }
+            println!();
+        }
+    };
+}
+
+fn main() {
+    let n: i32;
+    let mut a_v: Vec<String>;
+    scan!(n);
+    scanv!(n, a_v, String);
+    ptv!(a_v)
+}
