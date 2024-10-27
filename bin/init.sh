ffcb4f4fa5222a19384702602a535bd8fba8c6be
asaringo <asaringo99@gmail.com>
2024-10-28 02:08:36 +0900

    bin fix

--- bin/init.sh
+++ bin/init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ENVPATH="$SCRIPT_DIR/.env"
+
 # .envファイルを読み込む
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
+if [ -f $ENVPATH ]; then
+    export $(grep -v '^#' $ENVPATH | xargs)
 fi
 
 export PATH="$PATH:$COMPETITIVE_PROGRAMING_CONTEST_RUST_PROJECT_BIN_PATH"
